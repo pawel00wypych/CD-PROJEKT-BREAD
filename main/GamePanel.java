5a9686c4877f3cbe7ed477e7a154a8207fe01f87
Pawel Wypych <pawelwypych4@gmail.com>
2022-12-01 22:07:29 +0100

    Added KeyHandler and time to game loop

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -15,13 +15,22 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 60;
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+
+    int playerSpeed = 4;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
-
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -32,16 +41,52 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+
+        double drawInterval = 1000000000/FPS;
+
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while(gameThread != null) {
+
+            long currentTime = System.nanoTime();
 
             update();
 
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+
+                remainingTime /= 1000000;
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+
         }
     }
 
     public void update() {
-
+        if(keyH.upPressed) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -52,7 +97,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
 
--- main/Main.java
+++ main/Main.java
@@ -11,6 +11,7 @@ public class Main {
         window.setResizable(false);
         window.setTitle("2D Adventure");
 
+
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
