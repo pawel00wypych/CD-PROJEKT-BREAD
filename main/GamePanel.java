375afab058d8d765b1d93f2d583508c8453aa090
Pawel Wypych <pawelwypych4@gmail.com>
2022-12-02 10:57:54 +0100

    added new Entity and Player classes

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; //16x16
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -19,10 +21,7 @@ public class GamePanel extends JPanel implements Runnable{
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
-    int playerX = 100;
-    int playerY = 100;
-
-    int playerSpeed = 4;
+    Player player = new Player(this, keyH);
 
     public GamePanel() {
 
@@ -75,18 +74,7 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update() {
-        if(keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -95,9 +83,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
 
