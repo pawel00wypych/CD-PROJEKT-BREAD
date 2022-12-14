20a07cedb17232ddeeb1dbb7c19321a24398630f
Pawel Wypych <pawelwypych4@gmail.com>
2022-12-14 11:54:00 +0100

    added Game State

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -29,7 +29,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     // SYSTEM
     TileManager tileM = new TileManager(this);
-    KeyHandler keyH = new KeyHandler();
+    KeyHandler keyH = new KeyHandler(this);
     Thread gameThread;
 
     Sound music = new Sound();
@@ -47,8 +47,10 @@ public class GamePanel extends JPanel implements Runnable{
     //you can display only 10 objects at screen
     public SuperObject obj[] = new SuperObject[10];
 
-
-
+    // GAME STATE
+    public int gameState;
+    public final int playState = 1;
+    public final int pauseState = 2;
 
 
     public GamePanel() {
@@ -63,8 +65,8 @@ public class GamePanel extends JPanel implements Runnable{
     public void setupGame() {
 
         aSetter.setObject();
-
         playMusic(0);
+        gameState = playState;
     }
     public void startGameThread() {
 
@@ -108,7 +110,13 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update() {
-        player.update();
+
+        if(gameState == playState) {
+            player.update();
+
+        } else if (gameState ==pauseState) {
+            // nothing
+        }
     }
 
     public void paintComponent(Graphics g) {
