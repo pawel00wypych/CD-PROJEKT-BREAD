00cd206a7c96c58aa4c4af3a479ed3b1df9a7597
PonuryZniwiarz2k <karolwojtasz@gmail.com>
2022-12-19 20:04:45 +0100

    added collision checker for entities and refactored entity class

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,10 +1,16 @@
 package entity;
 
+import main.GamePanel;
+import main.UtilityTool;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Entity {
 
+    GamePanel gp;
     public int worldX, worldY;
     public int speed;
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
@@ -14,7 +20,7 @@ public class Entity {
 
     public int spriteNumber = 1;
 
-    public Rectangle solidArea;
+    public Rectangle solidArea = new Rectangle(0,0,48,48);
 
     // CHARACTER STATUS
     public int maxLife;
@@ -22,4 +28,99 @@ public class Entity {
 
     public int solidAreaDefaultX, solidAreaDefaultY;
     public  boolean collisionOn = false;
+    public int actionLockCounter = 0;
+    public Entity(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void setAction(){}
+
+    public void update(){
+        setAction();
+        collisionOn = false;
+        gp.colChecker.checkTile(this);
+        gp.colChecker.checkObject(this,false);
+        gp.colChecker.checkPlayer(this);
+        if(!collisionOn) {
+            switch (direction) {
+                case "up": worldY -= speed; break;
+                case "down": worldY += speed; break;
+                case "left": worldX -= speed; break;
+                case "right": worldX += speed; break;
+            }
+        }
+        spriteCounter++;
+        if (spriteCounter > 12) {
+            if (spriteNumber == 1) {
+                spriteNumber = 2;
+            } else if (spriteNumber == 2) {
+                spriteNumber = 1;
+            }
+            spriteCounter = 0;
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        BufferedImage image = null;
+
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+            switch (direction) {
+                case "up":
+                    if(spriteNumber == 1) {
+                        image = up1;
+                    }
+                    if(spriteNumber == 2) {
+                        image = up2;
+                    }
+                    break;
+                case "down":
+                    if(spriteNumber == 1) {
+                        image = down1;
+                    }
+                    if(spriteNumber == 2) {
+                        image = down2;
+                    }
+                    break;
+                case "left":
+                    if(spriteNumber == 1) {
+                        image = left1;
+                    }
+                    if(spriteNumber == 2) {
+                        image = left2;
+                    }
+                    break;
+                case "right":
+                    if(spriteNumber == 1) {
+                        image = right1;
+                    }
+                    if(spriteNumber == 2) {
+                        image = right2;
+                    }
+                    break;
+
+            }
+            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+        }
+
+    }
+
+    public BufferedImage setup(String imagePath) {
+
+        UtilityTool uTool = new UtilityTool();
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(imagePath+".png"));
+            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -12,19 +12,19 @@ import java.io.IOException;
 
 public class Player extends Entity{
 
-    GamePanel gp;
     KeyHandler keyH;
-
     public final int screenX;
     public final int screenY;
     public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
-        this.gp = gp;
+
+        super(gp);
         this.keyH = keyH;
 
         screenX = gp.screenWidth/2 - (gp.tileSize/2);
         screenY = gp.screenHeight/2 - (gp.tileSize/2);
+
         solidArea = new Rectangle(8,16,32,32);
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
@@ -48,32 +48,17 @@ public class Player extends Entity{
 
     public void getPlayerImage() {
 
-        up1 = setup("boy_up_1");
-        up2 = setup("boy_up_2");
-        down1 = setup("boy_down_1");
-        down2 = setup("boy_down_2");
-        left1 = setup("boy_left_1");
-        left2 = setup("boy_left_2");
-        right1 = setup("boy_right_1");
-        right2 = setup("boy_right_2");
+        up1 = setup("/player/Walking sprites/boy_up_1");
+        up2 = setup("/player/Walking sprites/boy_up_2");
+        down1 = setup("/player/Walking sprites/boy_down_1");
+        down2 = setup("/player/Walking sprites/boy_down_2");
+        left1 = setup("/player/Walking sprites/boy_left_1");
+        left2 = setup("/player/Walking sprites/boy_left_2");
+        right1 = setup("/player/Walking sprites/boy_right_1");
+        right2 = setup("/player/Walking sprites/boy_right_2");
     }
 
-    public BufferedImage setup(String imageName) {
 
-        UtilityTool uTool = new UtilityTool();
-        BufferedImage image = null;
-
-        try {
-
-            image = ImageIO.read(getClass().getResourceAsStream("/player/Walking sprites/"+imageName+".png"));
-            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return image;
-    }
     public void update() {
 
         if(keyH.upPressed || keyH.downPressed
